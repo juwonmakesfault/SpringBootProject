4847180643552762f0356bb711716719c488f094
kylin-1q84 <shadekj@naver.com>
2019-04-19 21:54:37 +0900

    Make httpconfig

--- src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/api/RiotApiClient.java
+++ src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/api/RiotApiClient.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class OpenWeatherMapApiClient {
+public class RiotApiClient {
 
     @Autowired
     private RestTemplate restTemplate;
