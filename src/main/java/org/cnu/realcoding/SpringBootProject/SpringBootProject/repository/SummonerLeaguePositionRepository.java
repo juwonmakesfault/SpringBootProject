65c4bcb2c8397f9daf16a67bf1ca3d92573a968f
kylin-1q84 <shadekj@naver.com>
2019-04-19 21:18:04 +0900

    Add import class

--- src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/repository/SummonerLeaguePositionRepository.java
+++ src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/repository/SummonerLeaguePositionRepository.java
@@ -1,5 +1,6 @@
 package org.cnu.realcoding.SpringBootProject.SpringBootProject.repository;
 
+import org.cnu.realcoding.SpringBootProject.SpringBootProject.domain.SummonerLeaguePosition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
