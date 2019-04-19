b20f516378107f551cd72eceec80fa997c69ac97
kylin-1q84 <shadekj@naver.com>
2019-04-19 20:50:25 +0900

    Make Frame for Controller.java

--- src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/controller/SummonerController.java
+++ src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/controller/SummonerController.java
@@ -1,4 +1,14 @@
 package org.cnu.realcoding.SpringBootProject.SpringBootProject.controller;
 
+import org.cnu.realcoding.SpringBootProject.SpringBootProject.service.SummonerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SummonerController {
+
+    @Autowired
+    private SummonerService summonerService;
+
+
 }
