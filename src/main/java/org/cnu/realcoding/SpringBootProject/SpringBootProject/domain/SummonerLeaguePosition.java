b5c64f1e7fe5c8378de59a29ec12c862f45b3152
kylin-1q84 <shadekj@naver.com>
2019-04-21 21:27:27 +0900

    Add position in domain

--- src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/domain/SummonerLeaguePosition.java
+++ src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/domain/SummonerLeaguePosition.java
@@ -15,4 +15,29 @@ public class SummonerLeaguePosition {
         private String id;
         private String acountid;
     }
+    private static class LeaguePositionDTO {
+        private String queueType;
+        private String summonerName;
+        private boolean hotStreak;
+        private MiniSeriesDTO miniSeriesDTO;
+        private int wins;
+        private boolean veteran;
+        private int losses;
+        private String rank;
+        private String leagueId;
+        private boolean inactive;
+        private boolean freshBlood;
+        private String leagueName;
+        private String position;
+        private String tier;
+        private String summonerId;
+        private String leaguePoints;
+    }
+    private static class MiniSeriesDTO{
+        private String progress;
+        private int losses;
+        private int target;
+        private int wins;
+    }
+
 }
