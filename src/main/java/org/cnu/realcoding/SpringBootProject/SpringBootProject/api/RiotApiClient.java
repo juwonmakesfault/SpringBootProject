ae10a1834202ad63f18a2f3318851b19273c233f
kylin-1q84 <shadekj@naver.com>
2019-04-21 20:57:53 +0900

    Change APIkey

--- src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/api/RiotApiClient.java
+++ src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/api/RiotApiClient.java
@@ -14,12 +14,13 @@ public class RiotApiClient {
     private RestTemplate restTemplate;
     private String requestNameurl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={api_key}";
     private String requestSummonerurl ="https://kr.api.riotgames.com/lol/league/v4/positions/by-summoner/{encryptedPUUID}?api_key={api_key}";
+    private String api_key = "RGAPI-bab5580b-6a94-44ee-a0e3-f1350e63d24a";
     public SummonerLeaguePosition getSummonerLeagueName(String summonerName){
-        return restTemplate.exchange(requestNameurl, HttpMethod.GET, null,SummonerLeaguePosition.class,summonerName,"RGAPI-d1c4b486-1d9d-422c-a2aa-f2a7534c6d3d")
+        return restTemplate.exchange(requestNameurl, HttpMethod.GET, null,SummonerLeaguePosition.class,summonerName,api_key)
                 .getBody();
     }
     public SummonerLeaguePosition getSummonerLeaguePosition(String summonerPUUID){
-        return restTemplate.exchange(requestSummonerurl, HttpMethod.GET, null,SummonerLeaguePosition.class,summonerPUUID,"RGAPI-d1c4b486-1d9d-422c-a2aa-f2a7534c6d3d")
+        return restTemplate.exchange(requestSummonerurl, HttpMethod.GET, null,SummonerLeaguePosition.class,summonerPUUID,api_key)
                 .getBody();
     }
 }
