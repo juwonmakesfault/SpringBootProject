0bb4a1d84f1dfc91b484a21b3a694be8a3b5079e
kylin-1q84 <shadekj@naver.com>
2019-04-21 20:45:45 +0900

    Modify RequestUrl

--- src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/api/RiotApiClient.java
+++ src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/api/RiotApiClient.java
@@ -13,7 +13,7 @@ public class RiotApiClient {
     @Autowired
     private RestTemplate restTemplate;
     private String requestNameurl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={api_key}";
-    private String requestSummonerurl ="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{encryptedPUUID}?api_key={api_key}";
+    private String requestSummonerurl ="https://kr.api.riotgames.com/lol/league/v4/positions/by-summoner/{encryptedPUUID}?api_key={api_key}";
     public SummonerLeaguePosition getSummonerLeagueName(String summonerName){
         return restTemplate.exchange(requestNameurl, HttpMethod.GET, null,SummonerLeaguePosition.class,summonerName,"RGAPI-d1c4b486-1d9d-422c-a2aa-f2a7534c6d3d")
                 .getBody();
