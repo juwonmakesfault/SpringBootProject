3d43854b6aabe8e90dd531f26fc9ca9fdb9b8d05
clearlyhunch <edgecoding@naver.com>
2019-05-02 20:49:15 +0900

    Add GetMapping Method

--- src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/controller/SummonerController.java
+++ src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/controller/SummonerController.java
@@ -1,5 +1,6 @@
 package org.cnu.realcoding.SpringBootProject.SpringBootProject.controller;
 
+import org.cnu.realcoding.SpringBootProject.SpringBootProject.domain.SummonerLeaguePosition;
 import org.cnu.realcoding.SpringBootProject.SpringBootProject.service.SummonerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,8 @@ public class SummonerController {
     @Autowired
     private SummonerService summonerService;
 
-    @GetMapping("/dogs/{name}")
-    public String getSummonerByName(@PathVariable String name) {
+    @GetMapping("/summoner/{name}")
+    public SummonerLeaguePosition.LeaguePositionDTO getSummonerByName(@PathVariable String name) {
         return summonerService.getSummonerLeaguePosition(name);
     }
 
