7f57130169b009de8ceb6a84786772ca98b6595d
ClearlyHunch <edgecoding@naver.com>
2019-05-02 20:56:53 +0900

    Add Controller (#7)
    * Change Api key
    
    * Change Api key
    
    * Modified import
    
    * Add SummonerName method
    
    * Change to use parameter
    
    * Add Return LeaguePositionDTO
    
    * Add GetMapping Method
    
    * Remove Schedule

--- src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/controller/SummonerController.java
+++ src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/controller/SummonerController.java
@@ -1,7 +1,10 @@
 package org.cnu.realcoding.SpringBootProject.SpringBootProject.controller;
 
+import org.cnu.realcoding.SpringBootProject.SpringBootProject.domain.SummonerLeaguePosition;
 import org.cnu.realcoding.SpringBootProject.SpringBootProject.service.SummonerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +12,12 @@ public class SummonerController {
 
     @Autowired
     private SummonerService summonerService;
+
+    @GetMapping("/summoner/{name}")
+    public SummonerLeaguePosition.LeaguePositionDTO getSummonerByName(@PathVariable String name) {
+        return summonerService.getSummonerLeaguePosition(name);
+    }
+
 
 
 }
--- src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/service/SummonerService.java
+++ src/main/java/org/cnu/realcoding/SpringBootProject/SpringBootProject/service/SummonerService.java
@@ -12,23 +12,20 @@ import java.util.List;
 
 @Service
 public class SummonerService {
-    private LinkedList<String> Summoner = new LinkedList();
+    private LinkedList<String> Summoner = new LinkedList<>();
 
     @Autowired
     private RiotApiClient riotApiClient;
     @Autowired
     private SummonerLeaguePositionRepository summonerLeaguePositionRepository;
 
-    @Scheduled(initialDelay = 5000L , fixedDelay = 2000L)
-    public void getSummonerLeaguePosition(){
-        String id = riotApiClient.getSummonerLeagueName("hide on bush").getId();
+    public SummonerLeaguePosition.LeaguePositionDTO getSummonerLeaguePosition(String summorerName){
+        String id = riotApiClient.getSummonerLeagueName(summorerName).getId();
         List<SummonerLeaguePosition.LeaguePositionDTO> summonerLeaguePosition = riotApiClient.getSummonerLeaguePosition(id);
-        for(SummonerLeaguePosition.LeaguePositionDTO a : summonerLeaguePosition)
-            summonerLeaguePositionRepository.insertSummonerLeaguePosition(a);
+        for(SummonerLeaguePosition.LeaguePositionDTO dto : summonerLeaguePosition)
+            summonerLeaguePositionRepository.insertSummonerLeaguePosition(dto);
+        return summonerLeaguePosition.get(0);
     }
-
-
-
 }
 
 
